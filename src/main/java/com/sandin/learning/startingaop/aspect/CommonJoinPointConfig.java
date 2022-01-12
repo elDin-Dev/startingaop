4c569c4de95f5a30eb5dc96ead31662c27e5ad81
sandina <alejandro.sandin@roche.com>
2022-01-12 14:12:05 +0100

    shorcuts para definir jointpoints.

--- src/main/java/com/sandin/learning/startingaop/aspect/CommonJoinPointConfig.java
+++ src/main/java/com/sandin/learning/startingaop/aspect/CommonJoinPointConfig.java
@@ -10,4 +10,9 @@ public class CommonJoinPointConfig {
     @Pointcut("execution(* com.sandin.learning.startingaop.business.*.*(..)))")
     public void businessLayerExecution(){}
 
+    @Pointcut("com.sandin.learning.startingaop.aspect.CommonJoinPointConfig.dataLayerExecution() && com.sandin.learning.startingaop.aspect.CommonJoinPointConfig.businessLayerExecution()")
+    public void allLayerExecution(){}
+
+    @Pointcut("bean(*Dao*)")
+    public void beanContainsDao(){}
 }
